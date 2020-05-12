95efdef62f7a619ab13f590912ea0f64193a6b1c
kittisak office <cda.kittisak@gmail.com>
2020-05-12 11:41:12 +0700

    start flutter

---
+++ class.dart
@@ -0,0 +1,25 @@
+class Dog {
+  int age;
+  String type;
+  String name;
+  double size;
+  // Dog(this.type, this.name, [this.age = 5, this.size = 10]);
+  void bark() {
+    print('หมาชื่อ xxx พันธ์ yyy อายุ zz เห่าว่า text');
+  }
+}
+
+class SmallDog extends Dog {
+  runAway() {
+    print('I am running ');
+  }
+}
+
+class SmallDog {
+  // const MAX_BODY_SIZE = 30;
+  isSmall 
+}
+
+// void main() {
+//   var dog1 = Dog(type: 'lll', name: 'dksfjalksjf');
+// }
